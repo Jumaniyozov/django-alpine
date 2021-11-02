59e59506d0943e7731efd0d24a415869ad87da36
Jumaniyozov <JumaniyozovSE@gmail.com>
2021-11-02 17:29:06 +0500

    init

---
+++ alp/assets/bundle.js
@@ -0,0 +1,13 @@
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.data('dropdown', () => ({
+    open: false,
+ 
+    toggle() {
+        this.open = ! this.open
+    }
+}))
+
+Alpine.start();
